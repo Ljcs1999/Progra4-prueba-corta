dfd2faef1aedf24e3b3bc6c3f80d0fbcfbc0a1b4
Ljcs1999 <Ljcs_1999@hotmail.com>
2022-11-28 17:54:42 -0800

    Se removio el controlador ping

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,9 +1,9 @@
-export * from './ping.controller';
-export * from './filtro-solicitante.controller';
-export * from './plazas.controller';
-export * from './requisitos.controller';
-export * from './solicitante.controller';
 export * from './filtro-solicitante-solicitante.controller';
-export * from './solicitante-filtro-solicitante.controller';
-export * from './requisitos-plazas.controller';
+export * from './filtro-solicitante.controller';
 export * from './plazas-requisitos.controller';
+export * from './plazas.controller';
+export * from './requisitos-plazas.controller';
+export * from './requisitos.controller';
+export * from './solicitante-filtro-solicitante.controller';
+export * from './solicitante.controller';
+
